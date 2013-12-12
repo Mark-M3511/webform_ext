66d4ecc537c05516464156e4834dca5ed1fecd03
Mark-M3511 <mark.mcfarlane@gmail.com>
2013-12-12 18:11:14 -0500

    Change Log 2013-12-12-A
    Code changes and clean up
    1: Added universal date error handling function
    2:  Removed uncessary form element arrays
    3:  Added  ‘#validatre’ property to “Submit” button
    4: Reafactored ajax call back function
    ‘ajax_load_available_rooms_callback’ to handle ajax submision errors
    caused by invalidate date submission
    5: Added ‘_webform_ext_check_date_errors’ function
    6: Added $is_reserved flag to allow designated button only to trigger
    calculation functions
    7: Added error handling code to ‘ajax_load_available_rooms_callback’
    8; Renamed ‘_webform_ext_reservation_form_1_validate’ to
    ‘_webform_ext_reservation_validate_reservation’ to match name of
    designated function assigned to submit form element
    9: Added universal function to load form errors
    10: Removed superfluous error logging calls inside cron queue callback
    funcrion
    11: Refactored jQuery code to properly handle error display on the form

--- js/webform_ext_forms.js
+++ js/webform_ext_forms.js
@@ -5,20 +5,37 @@
 		   if (Drupal.settings.we_errors_list === undefined){
 			  return; 
 		   }
-		   var err = Drupal.settings.we_errors_list.error_msgs;		   
+		   var err = Drupal.settings.we_errors_list.error_msgs;	
+		   var ctr = 0;
+		   var ul_element = "#block-system-main ul#errors";
+		   for (var prop in err){
+		      ctr++;
+		      if (window.console && console.log){
+				 console.log(ctr);
+			  }		      
+// 		      if (prop == ""){
+// 		         return;
+// 		      }
+		   }	
+		   if ($(ul_element).length > 0){
+		      $(ul_element).remove(); 
+		   }		   
+		   // There are no errors to process
+		   if (ctr == 0){
+		      return;
+		   }
 		   var ul = $('<ul id="errors"></ul>');
 		   var items = [];
-		   $("#block-system-main ul#errors").remove();
 		   $("#block-system-main").prepend(ul);
-		   $.each(err, function(idx, value){
-		        items.push('<li>'+ err[idx] + '</li>');
-				if(window.console && console.log){
+		   $.each(err, function(idx, value){		        
+				if (window.console && console.log){
 				   console.log(err[idx]);
 				}
+				items.push('<li>'+ err[idx] + '</li>');
 		     }
-		   );
-		   $("#block-system-main ul#errors").append(items.join(''));
-		   $("#block-system-main ul#errors").delay(12000).fadeOut(3000);
+		   );		   
+		   $(ul_element).append(items.join(''));
+		   $(ul_element).delay(12000).fadeOut(3000);
 		} 
 	}	
 	Drupal.behaviors.we_date_desc = {
