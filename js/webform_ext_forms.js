7e83bea0055e6db18cbcd8a423ae75ccb389b3e9
Mark-M3511 <mark.mcfarlane@gmail.com>
2013-12-15 15:32:20 -0500

    Change Log 2013-12-15-A
    Code Refactoring and Clean up
    1: Renumbered error messages
    2: Improved javascript code for error message display
    Note: Still trying to get a handle on the context and settings
    variables used in Drupal’s jQuery implementation using Drupal.behaviours

--- js/webform_ext_forms.js
+++ js/webform_ext_forms.js
@@ -2,45 +2,35 @@
 (function($, Drupal){
 	Drupal.behaviors.we_errors_list = { 
 		attach: function (context, settings) {
-		   if (Drupal.settings.we_errors_list === undefined){
-			  return; 
-		   }
-		   var err = Drupal.settings.we_errors_list.error_msgs;	
-		   var ctr = 0;
-		   var ul_element = "#block-system-main ul#errors";
-		   for (var prop in err){
-		      ctr++;
-		      if (window.console && console.log){
-				 console.log(ctr);
-			  }		      
-// 		      if (prop == ""){
-// 		         return;
-// 		      }
-		   }	
-		   if ($(ul_element).length > 0){
-		      $(ul_element).remove(); 
-		   }		   
-		   // There are no errors to process
-		   if (ctr == 0){
-		      return;
-		   }
-		   var ul = $('<ul id="errors"></ul>');
+		   if (settings.we_errors_list === undefined) { return; }
+		   var err = settings.we_errors_list.error_msgs;	
 		   var items = [];
-		   $("#block-system-main").prepend(ul);
-		   $.each(err, function(idx, value){		        
-				if (window.console && console.log){
-				   console.log(err[idx]);
-				}
-				items.push('<li>'+ err[idx] + '</li>');
-		     }
-		   );		   
-		   $(ul_element).append(items.join(''));
-		   $(ul_element).delay(12000).fadeOut(3000);
-		} 
+		   $.each(err, function(idx, value){
+		      if (value.indexOf("***") == -1){
+		   	  	 err[idx] = "*** " + value;
+		   	  	 items.push('<li><span style="font-style: italic;">Error Detected: ' + value + '</span></li>');				 
+		   	  }
+		   });
+		   if (window.console && console.log) { console.log(settings.we_errors_list.error_msgs) };
+		   if (window.console && console.log) { console.log(items); }
+// 		   Drupal.detachBehaviors($(this)); // must be accompanied by "detach" property declaration
+	       if (items.length == 0) { return; }
+	       
+	       var ul = $("#block-system-main ul#errors");
+	       if (ul.length > 0) { 
+	          ul.remove();
+	       }
+	       
+	       ul = $('<ul id="errors"></ul>');
+	       $("#block-system-main").prepend(ul);	      
+	       ul.append(items.join(''));
+	       ul.delay(12000).fadeOut(3000);
+		}
 	}	
 	Drupal.behaviors.we_date_desc = {
 		attach: function (context) {
-		   var descDiv = $(".container-inline-date .description", context);
+// 		   var descDiv = $(".container-inline-date .description", context);
+		   var descDiv = $(".container-inline-date .description");	
 		   if (descDiv !== undefined){
 		      descDiv.text("e.g. YYYY-MM-DD");
 		   }
